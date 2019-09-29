e963d632d3e0bd24f29a79616e91724d4ee8263a
Brunno Lorenzoni <lorenzoni.brunno@gmail.com>
2019-09-29 20:42:31 -0300

    fixing transaction

--- server/routes/transactionRoute.js
+++ server/routes/transactionRoute.js
@@ -1,8 +1,7 @@
 const router = require('express').Router();
 const transactionController = require('../controllers/transactionController');
 
-router.post('/transaction/favored/', transactionController.transferToFavored);
-router.post('/transaction/contact/', transactionController.transferToContact);
+router.post('/transaction/', transactionController.transaction);
 router.get('/user/:idUser/transactions/', transactionController.getAllTransactions);
 router.get('/user/:idUser/transaction/:idTransaction', transactionController.getOneTransaction);
 
